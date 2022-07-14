392dbfca033c73a8462c8bf7aa890d70d20119c6
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-07-14 19:39:01 -0400

    Created additional api Routes, html routes and additional functions for managing notes in lib/manageNotes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,14 @@
-const router = require("express").Router();
+const router = require('express').Router();
 const uniqid = require('uniqid');
-const { createNote, validateNote } = require('../lib/manageNotes');
+const { createNote, validateNote, findById, deleteNote } = require('../lib/manageNotes');
 const { notes } = require('../db/db.json');
 
-router.get("/notes", (req, res) => {
+
+router.get('/notes', (req, res) => {
     res.json(notes);
 })
 
-router.get("/notes/:id", (req, res) => {
+router.get('/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if (result) {
       res.json(result);
@@ -16,13 +17,22 @@ router.get("/notes/:id", (req, res) => {
     }
 });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
     req.body.id = uniqid();
     if (!validateNote(req.body)) {
       res.status(400).send("This note is not properly formatted.");
     } else {
       const note = createNote(req.body, notes);
       res.json(notes);
+    }
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const result = deleteNote(req.params.id, notes);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
     }
 });
 
