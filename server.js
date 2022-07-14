60a498e8514ffb24210ca185fdc9c2eb36c23ac4
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-07-13 21:04:19 -0400

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+const { notes } = require('./db/db.json');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.get('/api/notes', (req, res) => {
+    res.send(notes);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
