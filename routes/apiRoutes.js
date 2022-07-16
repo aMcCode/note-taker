f4f2115bbb8e56277520a19ee97fa9598b5ce5e7
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-07-16 10:04:14 -0400

    attempted to build delete route for notes. Works for database. Failing to delete from html.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,9 @@
+const path = require('path');
+const fs = require('fs')
 const router = require('express').Router();
 const uniqid = require('uniqid');
 const { createNote, validateNote, findById, deleteNote } = require('../lib/manageNotes');
 const { notes } = require('../db/db.json');
-
 
 router.get('/notes', (req, res) => {
     res.json(notes);
@@ -22,18 +23,25 @@ router.post('/notes', (req, res) => {
     if (!validateNote(req.body)) {
       res.status(400).send("This note is not properly formatted.");
     } else {
-      const note = createNote(req.body, notes);
+      createNote(req.body, notes);
       res.json(notes);
     }
 });
 
 router.delete('/notes/:id', (req, res) => {
-    const result = deleteNote(req.params.id, notes);
-    if (result) {
-      res.json(result);
-    } else {
+  console.log(req.params.id);
+  const notesArray = notes.filter(note => note.id != req.params.id);
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  const updatedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+   if (updatedNotes) {
+    console.log("updatedNotes: " + JSON.stringify(updatedNotes));
+    res.json(updatedNotes);
+   } else {
       res.send(404);
-    }
+   }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { notes } = require('./db/db.json');
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
@@ -10,6 +9,7 @@ const app = express();
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
